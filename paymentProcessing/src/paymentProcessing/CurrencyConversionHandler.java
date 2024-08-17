bae3a316d05e31fbbdc1eecc07b30495f166e2f7
AnanyaArekatla <2320030254@klh.edu.in>
2024-08-17 13:10:04 +0530

    Update CurrencyConversionHandler.java

--- paymentProcessing/src/paymentProcessing/CurrencyConversionHandler.java
+++ paymentProcessing/src/paymentProcessing/CurrencyConversionHandler.java
@@ -4,8 +4,8 @@ public class CurrencyConversionHandler extends PaymentHandler {
     @Override
     protected void handlePayment(Payment payment) {
         if (!"USD".equals(payment.getCurrency())) {
-            // Simulate currency conversion to USD
-            payment.setAmount(payment.getAmount() * 1.1); // Assume 1.1 is the conversion rate
+            
+            payment.setAmount(payment.getAmount() * 1.1); 
             payment.setCurrency("USD");
             System.out.println("Payment converted to USD.");
         } else {
