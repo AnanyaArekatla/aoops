1079989ab704d897dcc22c14c69b0903c820085a
AnanyaArekatla <2320030254@klh.edu.in>
2024-08-17 13:28:09 +0530

    Update FraudCheckHandler.java

--- paymentProcessing/src/paymentProcessing/FraudCheckHandler.java
+++ paymentProcessing/src/paymentProcessing/FraudCheckHandler.java
@@ -3,7 +3,7 @@ package paymentProcessing;
 public class FraudCheckHandler extends PaymentHandler {
     @Override
     protected void handlePayment(Payment payment) {
-        // Simulate fraud check
+        
         if (payment.getAmount() > 10000) {
             payment.setFraudulent(true);
             System.out.println("Payment flagged as fraudulent.");
