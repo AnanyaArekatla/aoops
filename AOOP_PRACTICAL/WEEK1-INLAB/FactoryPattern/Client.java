714329d428b29183e3877554a36036f9dd30dbdb
Arekatla Ananya <2320030254@klh.edu.in>
2024-09-14 13:46:30 +0530

    Update Client.java

--- AOOP_PRACTICAL/WEEK1-INLAB/FactoryPattern/Client.java
+++ AOOP_PRACTICAL/WEEK1-INLAB/FactoryPattern/Client.java
@@ -3,7 +3,7 @@ package com.FactoryPattern;
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		VehicleFactory obj = new CarFactory();
 		VehicleFactory obj1 = new MotorCycleFactory();
 		obj.createVehicle();
